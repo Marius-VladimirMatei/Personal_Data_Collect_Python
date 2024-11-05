96cd07ee00370701e1f369b2f3f96794e5577b6b
Marius-Vladimir Matei <052583@edu.szf.at>
2024-11-05 14:09:41 +0100

    input version

--- personal_data_collect_python.py
+++ personal_data_collect_python.py
@@ -1,1 +1,26 @@
 # Personal Data Collect System in Python
+
+
+print("1. Add new person")
+print("2. Search for a person")
+selection = input("Please select an option")
+
+
+if selection == '1':
+
+    print('Add new person - Enter the information for the new preson:\t')
+    first_name = input('First name:\t')
+    last_name = input('Last name:\t')
+    age = input('Age:\t')
+    adress_street = input('Street:\t')
+    adress_number = input('Number:\t')
+    birth_date = input('Birth date:\t')
+
+
+    new_person = {}
+    new_person['First name'] = first_name
+    new_person['Last name'] = last_namer
+    new_person['Age'] = age
+    new_person['Street'] = adress_street
+    new_person['Number'] = adress_number
+    new_person['Birth Date'] = birth_date
